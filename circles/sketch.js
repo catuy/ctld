0712ca40e324e7eaccfac58bfcd75f4a2b8a0e8c
catuy <cataldo.diego@gmail.com>
2021-02-18 02:27:37 -0300

    Update sketch.js

--- circles/sketch.js
+++ circles/sketch.js
@@ -3,16 +3,26 @@ function setup() {
   // let c = color('#312783');
   let fill_color = color('#312783');
   let stroke_color = color('#E6007E');
-  background(200);
+  let background_color = color('#eee');
+  let stroke_wight = 1;
+
+  background(background_color);
   fill(fill_color);
   stroke(stroke_color);
+  strokeWeight(stroke_wight);
+
 }
 
 function touchMoved() {
-  ellipse(mouseX, mouseY, 160, 160);
-  strokeWeight(1);
+
+  let object_width = 150;
+  let object_height = 150;
+
+  ellipse(mouseX, mouseY, object_width, object_height);
   return false;
 }
+
+
 save_canvas = function() {
   save();
 }
