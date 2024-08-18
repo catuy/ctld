4377b17c4d5112b5a6e77ae9665e2c5135ad2f53
Diego Cataldo <cataldo.diego@gmail.com>
2024-08-18 03:08:27 -0300

    .

--- prueba_2024/sketch.js
+++ prueba_2024/sketch.js
@@ -3,13 +3,14 @@ let elipssewidth;
 let paused = false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight); // Canvas al 100% de la pantalla
+  createCanvas(windowWidth, windowHeight);
   noStroke();
-  background(255); // Fondo blanco
+  background(255);
 
   x = width / 2;
   y = height / 2;
   elipssewidth = 80;
+
 }
 
 function draw() {
@@ -22,17 +23,26 @@ function draw() {
       y = mouseY;
     }
 
-    // Limita el movimiento al área del canvas
     x = constrain(x, elipssewidth / 2, width - elipssewidth / 2);
     y = constrain(y, elipssewidth / 2, height - elipssewidth / 2);
 
-    // Dibuja el patrón visual
-    let p = get(x - 50, y - 50, 200, 200);
-    p.filter(INVERT);
-    image(p, x, y, elipssewidth, elipssewidth);
+      let p = get(x - 50, y - 50, 200, 200);
+      p.filter(INVERT);
+      image(p, x, y, elipssewidth, elipssewidth);
+    } else {
+      fill(0);
+    }
 
     imageMode(CENTER);
   }
+
+function mousePressed() {
+  attemptInstall();
+}
+
+function touchStarted() {
+  attemptInstall();
+  return false;
 }
 
 function keyPressed() {
@@ -43,33 +53,19 @@ function keyPressed() {
   }
 }
 
-function touchStarted() {
-  paused = !paused;
-  return false;
-}
-
-function touchEnded() {
-  if (touches.length === 2) {
-    saveHighResImage();
-  }
-  return false;
-}
-
 function saveHighResImage() {
-  let scaleFactor = 5; // Factor de escala para alta resolución
+  let scaleFactor = 5;
   let highResCanvas = createGraphics(width * scaleFactor, height * scaleFactor);
   highResCanvas.noStroke();
-  highResCanvas.background(255); // Fondo blanco en la imagen de alta resolución
+  highResCanvas.background(255);
 
-  // Redibujar la escena en la imagen de alta resolución
   highResCanvas.translate(highResCanvas.width / width, highResCanvas.height / height);
   highResCanvas.image(get(), 0, 0, highResCanvas.width, highResCanvas.height);
 
-  // Guardar la imagen en alta resolución en Android
   highResCanvas.save(`myfile-${hour()}${minute()}${second()}_highres.jpg`);
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight); // Reajusta el canvas al tamaño completo
-  background(255); // Fondo blanco
+  resizeCanvas(windowWidth, windowHeight);
+  background(255);
 }
