706114fe07c6ddca33ab6b251a8d9183c8c5de96
Diego Cataldo <cataldo.diego@gmail.com>
2024-08-18 11:07:31 -0300

    Update sketch.js

--- prueba_20242/sketch.js
+++ prueba_20242/sketch.js
@@ -1,5 +1,5 @@
 let x, y;
-let ellipseWidth;
+let elipssewidth;
 let paused = false;
 
 function setup() {
@@ -9,44 +9,48 @@ function setup() {
 
   x = width / 2;
   y = height / 2;
-  ellipseWidth = 80;
-
-  // Botón de Pausa
-  let pauseButton = select('#pause-button');
-  pauseButton.mousePressed(togglePause);
-
-  // Botón de Guardar Imagen
-  let saveButton = select('#save-button');
-  saveButton.mousePressed(saveHighResImage);
+  elipssewidth = 80;
 }
 
 function draw() {
   if (!paused) {
-    if (!mouseIsPressed && touches.length === 0) {
+    if (!mouseIsPressed) {
       x += (noise(frameCount * 0.01) - 0.5) * 30;
       y += (noise(frameCount * 0.02) - 0.5) * 30;
-    } else if (touches.length === 1) {
-      x = touches[0].x;
-      y = touches[0].y;
+    } else {
+      x = mouseX;
+      y = mouseY;
     }
 
-    x = constrain(x, ellipseWidth / 2, width - ellipseWidth / 2);
-    y = constrain(y, ellipseWidth / 2, height - ellipseWidth / 2);
+    x = constrain(x, elipssewidth / 2, width - elipssewidth / 2);
+    y = constrain(y, elipssewidth / 2, height - elipssewidth / 2);
 
     let p = get(x - 50, y - 50, 200, 200);
     p.filter(INVERT);
-    image(p, x, y, ellipseWidth, ellipseWidth);
+    image(p, x, y, elipssewidth, elipssewidth);
 
     imageMode(CENTER);
   }
 }
 
-function togglePause() {
-  paused = !paused;
+function mousePressed() {
+  attemptInstall();
+}
+
+function touchStarted() {
+  attemptInstall();
+  return false;
+}
+
+function keyPressed() {
+  if (key === 's') {
+    saveHighResImage();
+  } else if (key === 'p') {
+    paused = !paused;
+  }
 }
 
 function saveHighResImage() {
   let scaleFactor = 5;
   let highResCanvas = createGraphics(width * scaleFactor, height * scaleFactor);
   highResCanvas.noStroke();
-  highResCanvas.background(255
