45c4f905a2d64965ae29379642729499e641a767
Diego Cataldo <cataldo.diego@gmail.com>
2024-08-18 11:01:51 -0300

    Update sketch.js

--- prueba_20242/sketch.js
+++ prueba_20242/sketch.js
@@ -11,13 +11,12 @@ function setup() {
   y = height / 2;
   ellipseWidth = 80;
 
-  // Crear botones en la interfaz
-  let pauseButton = createButton('Pausar');
-  pauseButton.position(10, 10);
+  // Botón de Pausa
+  let pauseButton = select('#pause-button');
   pauseButton.mousePressed(togglePause);
 
-  let saveButton = createButton('Guardar Imagen');
-  saveButton.position(10, 40);
+  // Botón de Guardar Imagen
+  let saveButton = select('#save-button');
   saveButton.mousePressed(saveHighResImage);
 }
 
@@ -42,15 +41,6 @@ function draw() {
   }
 }
 
-function mousePressed() {
-  attemptInstall();
-}
-
-function touchStarted() {
-  // Prevenir el comportamiento predeterminado para evitar conflictos
-  return false;
-}
-
 function togglePause() {
   paused = !paused;
 }
@@ -59,15 +49,4 @@ function saveHighResImage() {
   let scaleFactor = 5;
   let highResCanvas = createGraphics(width * scaleFactor, height * scaleFactor);
   highResCanvas.noStroke();
-  highResCanvas.background(255);
-
-  highResCanvas.translate(highResCanvas.width / width, highResCanvas.height / height);
-  highResCanvas.image(get(), 0, 0, highResCanvas.width, highResCanvas.height);
-
-  highResCanvas.save(`myfile-${hour()}${minute()}${second()}_highres.jpg`);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background(255);
-}
+  highResCanvas.background(255
