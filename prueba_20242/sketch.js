478a45bc1a3564607de7bd03c15a3c7b1394fe44
catuy <Uno43966>
2024-08-18 12:46:18 -0300

    update

--- prueba_20242/sketch.js
+++ prueba_20242/sketch.js
@@ -9,18 +9,13 @@ function setup() {
 
   x = width / 2;
   y = height / 2;
-  elipssewidth = 80;
+  elipssewidth = 30;
 }
 
 function draw() {
-  if (!paused) {
-    if (!mouseIsPressed) {
-      x += (noise(frameCount * 0.01) - 0.5) * 30;
-      y += (noise(frameCount * 0.02) - 0.5) * 30;
-    } else {
-      x = mouseX;
-      y = mouseY;
-    }
+  if (!paused && mouseIsPressed) {
+    x = mouseX;
+    y = mouseY;
 
     x = constrain(x, elipssewidth / 2, width - elipssewidth / 2);
     y = constrain(y, elipssewidth / 2, height - elipssewidth / 2);
@@ -33,21 +28,30 @@ function draw() {
   }
 }
 
-function mousePressed() {
-  attemptInstall();
-}
-
 function touchStarted() {
-  attemptInstall();
-  return false;
+  if (touches.length === 3) {
+    saveHighResImage();
+    return false; // Evita que el dibujo se realice al detectar tres toques
+  }
+  return true; // Permite el dibujo con uno o dos toques
 }
 
 function keyPressed() {
-  if (key === 's') {
-    saveCanvas(`myfile-${hour()}${minute()}${second()}`, 'jpg');
-  } else if (key === 'p') {
+  if (key === 'p') {
     paused = !paused;
   }
+}
+
+function saveHighResImage() {
+  let scaleFactor = 5;
+  let highResCanvas = createGraphics(width * scaleFactor, height * scaleFactor);
+  highResCanvas.noStroke();
+  highResCanvas.background(255);
+
+  highResCanvas.translate(highResCanvas.width / width, highResCanvas.height / height);
+  highResCanvas.image(get(), 0, 0, highResCanvas.width, highResCanvas.height);
+
+  highResCanvas.save(`myfile-${hour()}${minute()}${second()}_highres.jpg`);
 }
 
 function windowResized() {
